3cf43901e0533342c3dcd88963a4b84012759b7c
didi Work <diogo98antonio@gmail.com>
2025-05-29 16:52:31 -0300

    didiWork

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
-import { PostHeading } from '@/components/PostHeading';
+import { PostFeatured } from '@/components/PostFeatured';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
-import Image from 'next/image';
-import Link from 'next/link';
 import { Suspense } from 'react';
 
 export default function HomePage() {
@@ -12,37 +10,9 @@ export default function HomePage() {
         <Container>
             <Header />
 
-            <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
-                <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
-                    <Image
-                        className='w-full h-full object-cover object-center group-hover:scale-105 transition'
-                        src='/images/bryen_0.png'
-                        width={1200}
-                        height={720}
-                        alt='Título do post'
-                        priority
-                    />
-                </Link>
-                <div className='flex flex-col gap-4 sm:justify-center'>
-                    <time
-                        className='text-slate-600 block text-sm/tight'
-                        dateTime='2025-04-20'
-                    >
-                        20/04/2025 10:00
-                    </time>
-
-                    <PostHeading as='h1' url='#'>
-                        Rerum, vel ex? Impedit ullam harum blanditiis
-                    </PostHeading>
-
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
-                        dolorem est dolor porro, doloribus neque, quidem mollitia
-                        doloremque, ad perspiciatis fugiat. Rerum, vel ex? Impedit ullam
-                        harum blanditiis mollitia?
-                    </p>
-                </div>
-            </section>
+            <Suspense fallback={<SpinLoader />}>
+                <PostFeatured />
+            </Suspense>
 
             <Suspense fallback={<SpinLoader />}>
                 <PostsList />
