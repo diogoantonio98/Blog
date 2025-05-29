477fe0e18f2cf08ed72f0336aec860ceb0f3cf25
diogo home <diogo98antonio@gmail.com>
2025-05-28 21:25:06 -0300

    didiHome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
+import { PostHeading } from '@/components/PostHeading';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
 import Image from 'next/image';
@@ -14,18 +15,32 @@ export default function HomePage() {
             <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
                 <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
                     <Image
-                        className='group-hover:scale-105 transition'
+                        className='w-full h-full object-cover object-center group-hover:scale-105 transition'
                         src='/images/bryen_0.png'
                         width={1200}
                         height={720}
                         alt='Título do post'
+                        priority
                     />
                 </Link>
-                <div>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
-                    dolorem est dolor porro, doloribus neque, quidem mollitia doloremque,
-                    ad perspiciatis fugiat. Rerum, vel ex? Impedit ullam harum blanditiis
-                    mollitia?
+                <div className='flex flex-col gap-4 sm:justify-center'>
+                    <time
+                        className='text-slate-600 block text-sm/tight'
+                        dateTime='2025-04-20'
+                    >
+                        20/04/2025 10:00
+                    </time>
+
+                    <PostHeading as='h1' url='#'>
+                        Rerum, vel ex? Impedit ullam harum blanditiis
+                    </PostHeading>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
+                        dolorem est dolor porro, doloribus neque, quidem mollitia
+                        doloremque, ad perspiciatis fugiat. Rerum, vel ex? Impedit ullam
+                        harum blanditiis mollitia?
+                    </p>
                 </div>
             </section>
 
