5e019c00c08543c859e632b9f629d81deeb9c8c8
diogo home <diogo98antonio@gmail.com>
2025-05-29 21:22:42 -0300

    didiHome

---
+++ src/utils/format-datetime.ts
@@ -0,0 +1,19 @@
+import { format, formatDistanceToNow as dateFnsformatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
+export function formatDatetime(rawDate: string): string {
+    const date = new Date(rawDate);
+
+    return format(date, "dd/MM/yyyy 'às' HH'h'mm", {
+        locale: ptBR,
+    });
+}
+
+export function formatDistanceToNow(rawDate: string): string {
+    const date = new Date(rawDate);
+
+    return dateFnsformatDistanceToNow(date, {
+        locale: ptBR,
+        addSuffix: true,
+    });
+}
