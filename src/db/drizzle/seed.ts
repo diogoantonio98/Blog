a3583c66d8e2d527b913dad6fbe1dc20b0ce6bb3
didi Work <diogo98antonio@gmail.com>
2025-06-05 16:43:27 -0300

    didiWork

--- src/db/drizzle/seed.ts
+++ src/db/drizzle/seed.ts
@@ -3,21 +3,21 @@ import { drizzleDb } from '.';
 import { postsTable } from './schemas';
 
 (async () => {
-  const jsonPostRepository = new JsonPostRepository();
-  const posts = await jsonPostRepository.findAll();
+    const jsonPostRepository = new JsonPostRepository();
+    const posts = await jsonPostRepository.findAll();
 
-  try {
-    await drizzleDb.delete(postsTable); // ISSO LIMPA A BASE DE DADOS
-    await drizzleDb.insert(postsTable).values(posts);
+    try {
+        //await drizzleDb.delete(postsTable); // ISSO LIMPA A BASE DE DADOS
+        await drizzleDb.insert(postsTable).values(posts);
 
-    console.log();
-    console.log(`${posts.length} posts foram salvos na base de dados.`);
-    console.log();
-  } catch (e) {
-    console.log();
-    console.log('Ocorreu um erro...');
-    console.log();
-    console.log(e);
-    console.log();
-  }
+        console.log();
+        console.log(`${posts.length} posts foram salvos na base de dados.`);
+        console.log();
+    } catch (e) {
+        console.log();
+        console.log('Ocorreu um erro...');
+        console.log();
+        console.log(e);
+        console.log();
+    }
 })();
