9eb02c0faaf7f430310955eddd2e417805ef4ac7
didi Work <diogo98antonio@gmail.com>
2025-06-24 16:54:46 -0300

    didiWork

--- src/components/PostListAdmin/index.tsx
+++ src/components/PostListAdmin/index.tsx
@@ -1,7 +1,7 @@
 import { findAllPostAdmin } from '@/lib/post/queries/admin';
 import clsx from 'clsx';
-import { Trash2Icon } from 'lucide-react';
 import Link from 'next/link';
+import { DeletePostButton } from '../admin/DeletePostButton';
 
 export default async function PostsListAdmin() {
     const posts = await findAllPostAdmin();
@@ -26,20 +26,58 @@ export default async function PostsListAdmin() {
                             </span>
                         )}
 
-                        <button
-                            className={clsx(
-                                'text-red-500 cursor-pointer transition',
-                                '[&_svg]:w-4 [&_svg]:h-4',
-                                'hover:scale-120 hover:text-red-700',
-                            )}
-                            aria-label={`Apagar post: ${post.title}`}
-                            title={`Apagar post: ${post.title}`}
-                        >
-                            <Trash2Icon />
-                        </button>
+                        <DeletePostButton id={post.id} title={post.title} />
                     </div>
                 );
             })}
+
+            {/* start delete warn */}
+            <div
+                className={clsx(
+                    'fixed z-50 inset-0 bg-black/50 backdrop-blur-xs',
+                    'flex items-center justify-center',
+                )}
+            >
+                <div
+                    className={clsx(
+                        'bg-slate-100 p-6 rounded-lg max-w-2xl mx-6',
+                        'flex flex-col gap-6',
+                        'shadow-lg shadow-black/30 text-center',
+                    )}
+                >
+                    <h3 className='text-xl font-extrabold'>Título do diálogo</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
+                        ipsam quae laborum adipisci reprehenderit veniam delectus quos quas.
+                        Voluptates fuga, quisquam accusamus asperiores quam quo labore
+                        laborum modi similique quod?
+                    </p>
+                    <div className='flex items-center justify-around'>
+                        <button
+                            className={clsx(
+                                'bg-slate-200 hover:bg-slate-300 transition text-slate-950',
+                                'flex items-center justify-center',
+                                'py-2 px-4 rounded-lg cursor-pointer',
+                            )}
+                            autoFocus
+                        >
+                            Cancelar
+                        </button>
+
+                        <button
+                            className={clsx(
+                                'bg-blue-500 hover:bg-blue-600 transition text-blue-50',
+                                'flex items-center justify-center',
+                                'py-2 px-4 rounded-lg cursor-pointer',
+                            )}
+                        >
+                            Ok
+                        </button>
+                    </div>
+                </div>
+            </div>
+            {/* end delete warn */}
+
         </div>
     );
 }
