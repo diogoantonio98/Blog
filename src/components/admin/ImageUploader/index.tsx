aef07cf8f5112b2637df75ff5417243ea2d4475d
diogo home <diogo98antonio@gmail.com>
2025-06-29 14:43:45 -0300

    didiHome

--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -1,16 +1,57 @@
 'use client';
 
+import { uploadImageAction } from '@/action/upload/upload-image-action';
 import { Button } from '@/components/Button';
+import { IMAGE_UPLOAD_MAX_SIZE } from '@/lib/constants';
 import { ImageUpIcon } from 'lucide-react';
-import { useRef } from 'react';
+import { useRef, useTransition } from 'react';
+import { toast } from 'react-toastify';
 
 export function ImageUploader() {
     const fileInputRef = useRef<HTMLInputElement>(null);
+    const [isUploading, startTransition] = useTransition();
 
     function handleChooseFile() {
         if (!fileInputRef.current) return;
 
         fileInputRef.current.click();
+    }
+
+    function handleChange() {
+        toast.dismiss();
+
+        if (!fileInputRef.current) return;
+
+        const fileInput = fileInputRef.current;
+        const file = fileInput?.files?.[0];
+
+        if (!file) return;
+
+        if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
+            const readableMaxSize = IMAGE_UPLOAD_MAX_SIZE / 1024;
+            toast.error(`Imagem muito grande. Máx.: ${readableMaxSize}KB.`);
+
+            fileInput.value = '';
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('file', file);
+
+        startTransition(async () => {
+            const result = await uploadImageAction(formData);
+
+            if (result.error) {
+                toast.error(result.error);
+                fileInput.value = '';
+                return;
+            }
+
+            // TODO: continuar depois
+            toast.success(result.url);
+        });
+
+        fileInput.value = '';
     }
 
     return (
@@ -21,6 +62,7 @@ export function ImageUploader() {
             </Button>
 
             <input
+                onChange={handleChange}
                 ref={fileInputRef}
                 className='hidden'
                 name='file'
